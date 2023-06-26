13fd12984bf9c68994a47f1a3fbd3069779b8dc3
Luiz F Dias <luizfd@gmail.com>
2023-06-26 11:12:59 -0300

    Offer

--- src/main/java/com/devsuperior/DSLearn/entities/Course.java
+++ src/main/java/com/devsuperior/DSLearn/entities/Course.java
@@ -1,11 +1,14 @@
 package com.devsuperior.DSLearn.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class Course {
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
+	
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
 	
 	public Course() {}
 
@@ -58,6 +64,10 @@ public class Course {
 
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
+	}
+	
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
